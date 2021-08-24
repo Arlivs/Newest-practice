2b0edcbea7ce3c2e9a34266242dcfd8404df495f
Arlivs <ilyayev00@bk.ru>
2021-08-24 16:14:51 +0300

    upd

--- proj/serv.js
+++ proj/serv.js
@@ -8,34 +8,3 @@ function sendRequest(url, method, body = null) {
 // sendRequest(requestUrl)
 //     .then(data => console.log(data))
 //     .catch(err => console.error(err))
-
-
-const http = require("http")
-const url = require("url")
-const { parse } = require("querystring")
-
-http.createServer((request, response) => {
-    console.log('server work');
-    if (request.method == 'GET') {
-        console.log(request.method);
-        let urlRequest = url.parse(request.url, true);
-
-        console.log(urlRequest.query.test)
-        if (urlRequest.query.test % 2 == 0) response.end('even')
-        else response.end('odd')
-    }
-    else {
-        let body = '';
-        request.on('data', chunk => {
-            body += chunk.toString();
-        });
-        request.on('end', () => {
-            console.log(body);
-            let params = parse(body);
-            console.log(params);
-            console.log(params.hi);
-            response.end('ok');
-        });
-    }
-
-}).listen(3000);
